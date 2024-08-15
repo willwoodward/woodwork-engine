fd5849694d29733bd613ad20e35334342a514310
Will Woodward <will@woodwardweb.com>
2024-08-15 18:22:35 +0200

    Added config parsing functionality

--- src/components/knowledge_bases/vector_databases/chroma.py
+++ src/components/knowledge_bases/vector_databases/chroma.py
@@ -2,4 +2,8 @@ from components.knowledge_bases.vector_databases.vector_database import vector_d
 
 class chroma(vector_database):
     def __init__(self, name):
+        print("Initialising Chroma Knowledge Base...")
+        
         super().__init__(name)
+        
+        print(f"Chroma Knowledge Base {name} created.")
--- src/config_parser.py
+++ src/config_parser.py
@@ -1,22 +1,43 @@
+import re
+
 from components.component import component
 from components.knowledge_bases.vector_databases.chroma import chroma
 
 keywords = ["knowledge_base"]
 components: list[component] = []
 
-line = []
+commands = []
 with open("main.wf", "r") as f:
-    lines = list(map(lambda line: line.strip("\n"), f.readlines()))
+    lines = f.read()
+    
+    entry_pattern = r".+=.+\{[\s\S]*?\}"
+    entries = re.findall(entry_pattern, lines)
 
-for line in lines:
-    if "knowledge_base" in line:
-        # The name is everything before the equals sign with the whitespace removed either side
-        # Add error handling for spaces
-        name = line.split("=")[0].strip()
-        type = line.split("knowledge_base")[1].strip("{").strip("}").strip()
+    for entry in entries:
+        command = {}
+        # Replace these with some fancy regex
+        command["variable"] = entry.split("=")[0].strip()
+        command["component"] = entry.split("=")[1].split(" ")[1].strip()
+        command["type"] = entry.split(command["component"])[1].split("{")[0].strip()
         
-        if type == "chroma":
-            components.append(chroma(name))
+        config_items = list(map(lambda x: x.replace("\n", "").strip(), re.findall(r"\n[^\n\}]+", entry)))
+        config_items = [x for x in config_items if x != ""]
 
-for c in components:
-    print(c.name)
+        command["config"] = {}
+        for item in config_items:
+            key = item.split(":")[0].strip()
+            value = item.split(":")[1].strip()
+            command["config"][key] = value
+
+        commands.append(command)
+
+
+
+for command in commands:
+    if command["component"] == "knowledge_base":
+        
+        if command["type"] == "chroma":
+            components.append(chroma(command["variable"]))
+    
+
+print(components[0])
