c99768c3415ccd440f4e0129bc0011c7c019b58c
Will Woodward <will@woodwardweb.com>
2024-08-21 22:04:51 +0100

    Included dependencies from requirements

--- build/lib/woodwork/__main__.py
+++ build/lib/woodwork/__main__.py
@@ -1,7 +1,18 @@
 from woodwork.config_parser import main_function
+from woodwork.dependencies import init, activate_virtual_environment
+
+import sys
 
 def main():
-    main_function()
+    # woodwork
+    if len(sys.argv) == 1:
+        activate_virtual_environment()
+        main_function()
     
-if __name__ == "__main__":
-    main()
+    # woodwork init
+    elif sys.argv[1] == "init":
+        if len(sys.argv) == 2:
+            init()
+        else:
+            if sys.argv[2] == "--isolated":
+                init({"isolated": True})
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='woodwork',
-    version='0.0.1',
     packages=find_packages(where='.'),
     package_dir={'': '.'},
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'woodwork=woodwork.__main__:main',
         ],
     },
+    python_requires='>=3.10'
 )
