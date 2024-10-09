0ee837dbe202244170a87e17de485065b02fb8af
Will Woodward <will@woodwardweb.com>
2024-10-09 16:36:10 +0100

    initialised memory class

---
+++ woodwork/components/memory/memory.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from woodwork.components.component import component
+
+class memory(component, ABC):
+    def __init__(self, name, config):
+        super().__init__(name, "memory")
+    
+    @abstractmethod
+    @property
+    def data(self):
+        pass
+    
+    @abstractmethod
+    def add(self, text: str):
+        pass
---
+++ woodwork/components/memory/short_term.py
@@ -0,0 +1,7 @@
+from woodwork.components.memory.memory import memory
+
+class short_term(memory):
+    def __init__(self, name, config):
+        super().__init__(name, config)
+        
+        
