58ee7b2492fc69ac2cbcd7e6f5c95ddd7f303ca3
Will Woodward <will@woodwardweb.com>
2024-10-11 13:48:22 +0100

    initialised neo4j class

--- examples/02-function-tooling-agent/functions.py
+++ examples/02-function-tooling-agent/functions.py
@@ -7,3 +7,8 @@ def word_length(word: str):
     """A function that takes a string as input and returns its length as an integer."""
     
     return len(word)
+
+def sort_list(list: list[any]):
+    """A function that takes a list and returns the sorted list."""
+    
+    return sorted(list)
--- woodwork/components/knowledge_bases/graph_databases/neo4j.py
+++ woodwork/components/knowledge_bases/graph_databases/neo4j.py
@@ -1,9 +1,22 @@
+from neo4j import GraphDatabase
+
 from woodwork.components.knowledge_bases.graph_databases.graph_database import graph_database
 
 class neo4j(graph_database):
     def __init__(self, name, config):
         print("Initialising Neo4j Knowledge Base...")
         
-        super().__init__(name)
+        super().__init__(name, config)
+        
+        self._config_checker(name, ["uri", "user", "password"], config)
+        self._driver = GraphDatabase.driver(config["uri"], auth=(config["user"], config["password"]))
         
         print(f"Neo4j Knowledge Base {name} created.")
+        
+    def close(self):
+        self._driver.close()
+
+    def run(self, query, parameters=None):
+        with self.driver.session() as session:
+            result = session.run(query, parameters)
+            return result.data()
