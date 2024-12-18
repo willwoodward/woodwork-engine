4211477e78ab2f31caecde96209d20907efb2a8e
Will Woodward <will@woodwardweb.com>
2024-12-18 22:36:03 +0000

    started functionality for the custom tooling

---
+++ examples/05-custom-tool-agent/paper_search_tool.py
@@ -0,0 +1,18 @@
+from woodwork import tool_interface
+
+class PaperSearchTool(tool_interface):
+    def __init__(self):
+        pass
+    
+    def search(self, query):
+        return
+    
+    @property
+    def description(self):
+        return """Contains functionality to interact with papers on arxiv. The following functions can be used, with their inputs:
+        search(query) - Returns a result
+        To use these functions, specify only the function name as the action, and pass the key word arguments in the inputs dictionary.
+    """
+    
+    def input(action: str, inputs: dict):
+        return "Output of the input"
---
+++ woodwork/__init__.py
@@ -0,0 +1,3 @@
+from woodwork.interfaces.tool_interface import tool_interface
+
+__all__ = ["tool_interface"]
