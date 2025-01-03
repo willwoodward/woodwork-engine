29b03b2a01b719e104b1dbe6c956f272df0962f7
Will Woodward <will@woodwardweb.com>
2025-01-03 13:01:50 +0000

    added test scaffolding

---
+++ tests/parser_test.py
@@ -0,0 +1,39 @@
+# Testing component name declaration
+def test_same_name_as_keyword():
+    raise NotImplementedError
+
+def test_same_name_as_other_variable():
+    raise NotImplementedError
+
+def test_double_equals():
+    raise NotImplementedError
+
+# Testing keyword parsing
+def test_keywords_parsed():
+    raise NotImplementedError
+
+# Testing component properties
+def test_properties():
+    raise NotImplementedError
+
+# Testing component values
+def test_string_values():
+    raise NotImplementedError
+
+def test_variable_values():
+    raise NotImplementedError
+
+def test_list_values():
+    raise NotImplementedError
+
+def test_list_variable_values():
+    raise NotImplementedError
+
+def test_dictionary_values():
+    raise NotImplementedError
+
+def test_environment_values():
+    raise NotImplementedError
+
+def test_boolean_values():
+    raise NotImplementedError
