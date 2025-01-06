97bfa821eb71db2713994f6cdd14c95a2695c298
willwoodward <will@woodwardweb.com>
2025-01-06 15:06:43 +0000

    changed package info

--- setup.py
+++ setup.py
@@ -10,4 +10,9 @@ setup(
         ],
     },
     python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
 )
