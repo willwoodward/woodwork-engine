b72ac24011960f63e4fd73eaca280423f5cc78fd
willwoodward <will@woodwardweb.com>
2025-07-08 23:17:36 +0100

    loaded shadcn example dashboard

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button"
+import Page from "@/app/dashboard/page"
 
 function App() {
+  console.log(Page)
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Page />
   )
 }
 
